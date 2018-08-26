c7f92dad2ebb6c23ac429c55c542e94d8672a3b0
Samuel <sollysamuel12@gmail.com>
2018-08-26 20:34:54 +0100

    New Changes Made

--- form/conn.php
+++ form/conn.php
@@ -1,7 +1,13 @@
 <?php
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
 
+$hostname = $dbparts['sulnwdk5uwjw1r2k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'];
+$username = $dbparts['go6qc538216kmr69'];
+$password = $dbparts['m8hvtk2fo5l5i1qv'];
+$database = ltrim($dbparts['pgkyawhr0bkj5mno'],'/');
 
-$conn = mysqli_connect('sulnwdk5uwjw1r2k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'go6qc538216kmr69', '		m8hvtk2fo5l5i1qv', 'pgkyawhr0bkj5mno');
+$conn = mysqli_connect($hostname, $username, $password, $database);
 
 if(!$conn) {
     echo "connection failed:". mysqli_connect_error();
